f4672ead7f8f2abd36c03844bb24b24516f07341
TWRT <TWRT@DESKTOP-89MV5IT>
2018-06-01 11:22:43 +0900

    test2018

---
+++ java_silver/src/org/chapter1/Cxample01.java
@@ -0,0 +1,10 @@
+package org.chapter1;
+
+public class Cxample01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- java_silver/src/org/chapter1/Sample.java
+++ java_silver/src/org/chapter1/Sample.java
@@ -2,4 +2,7 @@ package org.chapter1;
 
 public class Sample {
 	public final static int VALUE = 100;
+	int a =0;
+	
+	
 }
